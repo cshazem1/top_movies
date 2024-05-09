c12bd00f2f22d762b7ecf7fa0a6458de463b288c
AQ <cshazemelwazery@gmail.com>
2024-05-09 21:23:50 +0300

    finish app

--- lib/core/utils/service_located.dart
+++ lib/core/utils/service_located.dart
@@ -1,12 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:top_movies/core/utils/api_service.dart';
-import 'package:top_movies/features/home/data/repo/home_repo.dart';
 import 'package:top_movies/features/home/data/repo/home_repo_imp.dart';
 
 final getIt = GetIt.instance;
 
 void setup() {
-  getIt.registerSingleton<HomeRepoImp>(HomeRepoImp(  getIt.get<ApiService>(),));
-  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<ApiService>(ApiService(Dio())); // Register ApiService first
+  getIt.registerSingleton<HomeRepoImp>(HomeRepoImp(getIt.get<ApiService>())); // Then register HomeRepoImp
 }
